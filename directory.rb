45cbc7042c1a0ff0da4942aa6e13266f980ba9df
Fitsum Teklehaimanot <fitsum@Fitsums-MacBook-Pro.local>
2014-05-13 13:47:05 +0100

    iterate

--- directory.rb
+++ directory.rb
@@ -1,27 +1,35 @@
 #first we print the list of students
 
 student_count = 19 # set variable to number of students
+
+#Array
+students = ["Nadav Matalon",
+"Fitsum Teklehaimanot",
+"Roi Driscoll",
+"James Whyte",
+"Margherita Serino",
+"Vaidas Mykolaitis",
+"Johann Bornman",
+"Kate Hamilton",
+"James Keap",
+"Nic Yeeles",
+"Julie Walker",
+"Will Allen",
+"Julia Tan",
+"Federico Maffei",
+"Jamie Patel",
+"Faezrah Rizalman",
+"Josh Fail-Brown",
+"Sasha Cooper",
+"Nicolai DTH"]
 puts "The students of my cohort at Makers Academy"
 puts"---------------------------------------------"
-puts"Nadav Matalon"
-puts"Fitsum Teklehaimanot"
-puts"Roi Driscoll"
-puts"James Whyte"
-puts"Margherita Serino"
-puts"Vaidas Mykolaitis"
-puts "Johann Bornman"
-puts"Kate Hamilton"
-puts"James Keap"
-puts"Nic Yeeles"
-puts"Julie Walker"
-puts"Will Allen"
-puts"Julia Tan"
-puts"Federico Maffei"
-puts"Jamie Patel"
-puts"Faezrah Rizalman"
-puts"Josh Fail-Brown"
-puts"Sasha Cooper"
-puts"Nicolai DTH"
+
+#iterating through the array students and printing each student
+students.each do |student|
+	puts student
+end
+
 
 #finally, we print th total, we call the variable by #{variable}
-puts "Overall, we have #{student_count} students."
+puts "Overall, we have #{students.length} students."
