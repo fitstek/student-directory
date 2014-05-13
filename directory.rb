282ff2a8d49fc4cb3f270feb3f391f661196db5a
Fitsum Teklehaimanot <fitsum@Fitsums-MacBook-Pro.local>
2014-05-13 17:49:17 +0100

    interactive student name inputting program

--- directory.rb
+++ directory.rb
@@ -1,25 +1,54 @@
-#first we print the list of students
+# create an empty array
+@students = []
+def interactive_menu
+	
+	loop do
+		
+		#prints the choices
+		puts "1. Input the students"
+		puts "2. Show the students"
+		puts "9. Exit"
 
-#Array
-def input_students
-	puts "Please enter the names of the students."
-	puts "To finish, just hit return"
-	# create an empty array
-	students = []
+		# read user input
+		selection = gets.chomp
 
-	#get the first name
-	name = gets.chomp
-
-	#while the name is not empty do the following(if return hit twice exit)
-	while !name.empty? do
-		students << {:name => name, :cohort => :november}
-		puts "Now we have #{students.length} students"
-		name = gets.chomp
+		# continue based on input
+		case selection
+			when "1"
+			input_students
+		
+			when "2"
+			print_header
+			print
+			print_footer
+			
+			when "9"
+			exit
+			else
+			puts"I don't know what you meant, try again"
+		
+		end
 	end
-students
 end
 
-students = input_students
+
+def input_students
+				puts "Please enter the names of the students."
+				puts "To finish, just hit return"
+				
+
+				#get the first name
+				name = gets.chomp
+				
+				#while the name is not empty do the following(if return hit twice exit)
+				while !name.empty? do
+					@students << {:name => name, :cohort => :May}
+					puts "Now we have #{@students.length} students"
+					name = gets.chomp
+				end
+				@students
+		end
+
 #method to print header
 def print_header
 	puts "The students of my cohort at Makers Academy"
@@ -27,18 +56,16 @@ def print_header
 end
 
 #method to print names
-def print(students)
-	students.each do |student|
+def print
+	@students.each do |student|
 		puts "#{student[ :name]} (#{student[:cohort]} cohort)"
 	end
 end
 
 #method to print the footer
-def print_footer(names)
-	puts "Overall, we have #{names.length} students."
+def print_footer
+	puts "Overall, we have #{@students.length} students."
 end
 
-print_header
-print(students)
-print_footer(students)
+interactive_menu
 
