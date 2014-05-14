a51947e41f30f26fb543d4f5921e59d29aa4b1b0
Fitsum Teklehaimanot <fitsum@Fitsums-MacBook-Pro.local>
2014-05-14 05:41:33 +0100

    take in cohort name

--- directory.rb
+++ directory.rb
@@ -1,50 +1,70 @@
-# create an empty array
+# create global variables
 @students = []
+@name = ""
+@cohort = ""
+
+#method for the interactive menu
 def interactive_menu
-	
 	loop do
 		
 		#prints the choices
 		print_menu
 
-		# read user input
+		# reads user input into the process method
 		process(gets.chomp)	
 	end
 end
 
+#method for the main engine(selection results)
 def process(selection)
 	case selection
 			when "1"
 				input_students
-		
 			when "2"
-				show_students
-			
+				show_students			
 			when "9"
 				save_students
 				exit
 			else
-				puts "I don't know what you meant, try again"
-		
+				puts "I don't know what you meant, try again"		
 		end
 end
 
+#method for inputting students data
 def input_students
-				puts "Please enter the names of the students."
-				puts "To finish, just hit return"
+	get_information
 				
+	#while the name is not empty do the following(if return hit twice exit)
+	while !@name.empty? do
 
-				#get the first name
-				name = gets.chomp
-				
-				#while the name is not empty do the following(if return hit twice exit)
-				while !name.empty? do
-					@students << {:name => name, :cohort => :May}
-					puts "Now we have #{@students.length} students"
-					name = gets.chomp
-				end
-				@students
-		end
+		#append student with the Hash values
+		@students << {:name => @name, :cohort => @cohort}
+
+		#print out number of students
+		puts "Now we have #{@students.length} students"
+		
+		get_information
+	end
+
+	# return students
+	@students
+end
+
+def get_information
+
+	#get the first name
+	puts "Please enter the names of the students."
+	puts "To finish, just hit return twice"
+	@name = gets.chomp
+
+	# read in cohort name
+	puts "Please enter cohort name"
+	@cohort = gets.chomp
+
+	#return name and cohort
+	@name
+	@cohort
+end
 
 #method to print header
 def print_header
@@ -52,7 +72,7 @@ def print_header
 	puts"---------------------------------------------"
 end
 
-#method to print names
+#method to print names by iterating over the array
 def print_students
 	@students.each do |student|
 		puts "#{student[ :name]} (#{student[:cohort]} cohort)"
@@ -64,32 +84,41 @@ def print_footer
 	puts "Overall, we have #{@students.length} students."
 end
 
+#method to print out the choices
 def print_menu
 	puts "1. Input the students"
 	puts "2. Show the students"
 	puts "9. Exit"
 end
 
+#method to print out the results
 def show_students
 	print_header
 	print_students
 	print_footer
 end
 
+#method to save the list of students in a file
 def save_students
 
-	#open file for writing
+	#open file for writing and save the reference(name of file, mode(r,w)) to it in file
 	file = File.open("students.csv", "w")
 
+	#iterate over the student list 
 	@students.each do |student|
+
+		#take the entries of the hash and convert them into arrays and store them in student_data
 		student_data = [student[:name], student[:cohort]]
+
+		#take the array elements of student_data and join them by using a comma
 		csv_line = student_data.join(",")
+
+		#take the joined elements and print them out in the file created
 		file.puts csv_line
 	end
 	file.close
 end
 	
-
-
+#load the menu first
 interactive_menu
 
